29855ec8c9aa74efe56199183e9a2cfb732afd62
Nicolas Janik <nicolas.janik@gmail.com>
2019-09-01 22:56:41 -0400

    New version: Can NOW send commands to the heatpump!

--- hayward-poolheater-mqtt.ino
+++ hayward-poolheater-mqtt.ino
@@ -5,31 +5,73 @@
 
 #include "my_config.h";
 
+#define DEBUG 1
+
+
 const char * ssid = SSID;
 const char * password = WIFIPASS;
 const char * mqtt_server = MQTT_HOST;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-long lastMsg = 0;
-char msg[10];
 
-bool inter = false;
+#define PIN D5
+
+#define COOL B00000000
+#define HEAT B00001000
+#define AUTO B00000100
+
+float cmdTemp;
+byte cmdMode;
+boolean cmdPower;
+
+float currentProgTemp = 0;
+
+float currentTempOut = 0;
+//float currentTempIn = 0;      //TODO
+byte currentMode = 255;
+boolean currentPower = false;
+
+boolean isProcessingCmd = false;
+
+byte defaultMaskPowerMode = 96;
+byte defaultMaskTemp = 2;
+
+//static CMD trame values
+unsigned char cmdTrame[12] = {
+  129,  //0 - HEADER
+  141,
+  96,   //2 - POWER & MODE
+  6,
+  2,    //4 - TEMP
+  30,
+  188,
+  188,
+  188,
+  188,
+  0,
+  0,    //11 - CHECKSUM
+};
 
 byte state = 0;
-uint16_t highCpt = 0;
-
+byte highCpt = 0;
 byte buffer = 0;
 byte cptBuffer = 0;
 byte wordCounter = 0;
-
 byte trame[40] = {};
 
-byte power = 0;
-float temp_out = 0;
-float temp_programmed = 0;
-byte mode = 0;
-byte automatic_mode = 0;
+
+char* modeToString(byte mode) {
+  if (mode == HEAT) {
+    return "HEAT";
+  } else if (mode == COOL) {
+    return "COOL";
+  } else if (mode == AUTO) {
+    return "AUTO";
+  }
+  return "unknown_mode";
+}
+
 
 void setup_wifi() {
 
@@ -52,14 +94,22 @@ void setup_wifi() {
 }
 
 
-
-void reconnect() {
+void MQTT_reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     if (client.connect(MQTT_CLIENT_NAME, MQTT_USER, MQTT_PASS)) {
       Serial.println("connected");
       client.publish("pool", "connected");
+      client.loop();
+
+      client.subscribe("pool/set_power_on");
+      client.loop();
+      client.subscribe("pool/set_power_off");
+      client.loop();
+      client.subscribe("pool/set_temp");
+      client.loop();
+      client.subscribe("pool/set_mode");
       client.loop();
 
     } else {
@@ -71,58 +121,344 @@ void reconnect() {
   }
 }
 
-void setup() {
-  Serial.begin(115200);
-  setup_wifi();
-  Serial.println("Hayward-pool-heater-mqtt starting...");
-  pinMode(D5, INPUT);
-  delay(500);
-  client.setServer(mqtt_server, 1883);
-}
+boolean mqttMsgReceivedCallBack(char* topic, byte* payload, unsigned int length) {
 
-byte checksum(byte trame[], byte size) {
-
-  int total = 0;
-  for (int i = 0; i < (size - 1); i++) {
-    total += Bit_Reverse(trame[i]);
+  #ifdef DEBUG
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  Serial.print("length: ");
+  Serial.println(length);
+  Serial.print("Value: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
   }
-  return (total - 3) % 255;
+  Serial.println();
+  #endif
+
+	if(isProcessingCmd) {
+		//A processing cmd is already running. Ignoring this one.
+		return false;
+	}
+
+  isProcessingCmd = true;
+
+
+
+  if (strcmp(topic, "pool/set_power_on") == 0) {
+    cmdPower = true;
+    cmdMode = currentMode;
+    cmdTemp = currentProgTemp;
+
+  } else if(strcmp(topic, "pool/set_power_off") == 0) {
+    cmdPower = false;
+    cmdMode = currentMode;
+    cmdTemp = currentProgTemp;
+
+  } else if(strcmp(topic, "pool/set_mode") == 0) {
+    payload[length] = '\0';
+    if (strcmp((char *) payload, "AUTO") == 0) {
+      cmdMode = AUTO;
+    } else if(strcmp((char *) payload, "COOL") == 0) {
+      cmdMode = COOL;
+    } else if(strcmp((char *) payload, "HEAT") == 0) {
+      cmdMode = HEAT;
+    }
+    cmdPower = currentPower;
+    cmdTemp = currentProgTemp;
+
+  } else if(strcmp(topic, "pool/set_temp") == 0) {
+    payload[length] = '\0';
+    String s = String((char*)payload);
+    float temp = s.toFloat();
+
+    if(temp >= 15 && temp <= 33) {
+      cmdTemp = temp;
+      cmdPower = currentPower;
+      cmdMode = currentMode;
+    }
+  } else {
+    isProcessingCmd = false;
+    return false;
+  }
+
+
+
+  #ifdef DEBUG
+  Serial.print("cmdPower: ");
+  Serial.println(cmdPower);
+  Serial.print("cmdMode: ");
+  Serial.println(modeToString(cmdMode));
+  Serial.print("cmdTemp: ");
+  Serial.println(cmdTemp);
+  #endif
+
+
+  if(cmdTemp == currentProgTemp && cmdMode == currentMode && cmdPower == currentPower) {
+    isProcessingCmd = false;
+    return false;
+  }
+
+  if(currentMode == 255 || currentProgTemp == 0) {  //255 is a dummy value that i've used to init the mode
+    //if we are here, we still don't have all of the current parameter. We can't send a cmd without knowing all current parameters
+    isProcessingCmd = false;
+    #ifdef DEBUG
+    Serial.println("Can't send cmd. Waiting for all current parameters");
+    #endif
+    return false;
+  }
+
+  Serial.println("prepareCmdTrame");
+  prepareCmdTrame();
+  Serial.println("sendCmdTrame");
+  sendCmdTrame();
+  Serial.println("resetRecevingTrameProcess");
+  resetRecevingTrameProcess();
+  isProcessingCmd = false;
 }
+
+
+void resetRecevingTrameProcess() {
+  pinMode(PIN, INPUT);
+  state = 0;
+  highCpt = 0;
+  cptBuffer = 0;
+  buffer = 0;
+  wordCounter = 0;
+  memset(trame, 0, sizeof(trame));
+}
+
+void resetTempAndPowerModeMask() {
+  cmdTrame[2] = defaultMaskPowerMode;
+  cmdTrame[4] = defaultMaskTemp;
+}
+
+
+void prepareCmdTrame() {
+  resetTempAndPowerModeMask();
+  setPowerInTrame(cmdPower);
+  setModeInTrame(cmdMode);
+  setTempInTrame(cmdTemp);
+  generateChecksumInTrame();
+}
+
+
+void sendCmdTrame() {
+
+  pinMode(PIN, OUTPUT);
+  //repeat the trame 8 times
+	for (byte occurrence = 0; occurrence < 8; occurrence++) {
+    yield();
+		sendHeaderCmdTrame();
+		for (byte trameIndex=0; trameIndex <= 11; trameIndex++) {
+			byte value = cmdTrame[trameIndex];
+			for (byte bitIndex = 0; bitIndex < 8; bitIndex++) {
+        byte bit = (value << bitIndex) & B10000000;
+				if(bit) {
+					sendBinary1();
+				} else {
+					sendBinary0();
+				}
+			}
+		}
+
+		if(occurrence < 7) {
+			sendSpaceCmdTrame();
+		} else {
+			sendSpaceCmdTramesGroup();
+      Serial.println("Cmd trame sent");
+		}
+	}
+}
+
+
+void sendBinary0() {
+	_sendLow(1);
+	_sendHigh(1);
+}
+
+void sendBinary1() {
+	_sendLow(1);
+	_sendHigh(3);
+}
+
+
+void sendHeaderCmdTrame() {
+	_sendLow(9);
+	_sendHigh(5);
+}
+
+
+//between trame repetition
+void sendSpaceCmdTrame(){
+	_sendLow(1);
+	_sendHigh(100);
+}
+
+//after the 8th trame sent
+void sendSpaceCmdTramesGroup() {
+	_sendLow(1);
+  //to avoid software watchdog reset due to the long 2000ms delay, we cut the 2000ms in 4x500ms and feed the wdt each time.
+  for(byte i=0; i<4 ;i++) {
+	 _sendHigh(500);
+   ESP.wdtFeed();
+ }
+}
+
+
+void _sendHigh(word ms) {
+	digitalWrite(PIN, HIGH);
+	delayMicroseconds(ms * 1000);
+}
+
+void _sendLow(word ms) {
+	digitalWrite(PIN, LOW);
+	delayMicroseconds(ms * 1000);
+}
+
+
+bool setTempInTrame(float temperature) {
+  byte temp = temperature;
+  bool halfDegree = ((temperature * 10) - (temp * 10)) > 0;
+
+  if(temp < 15 || temp > 33) {
+    Serial.println("Error setTemp: Value must be between 15 & 33");
+    return false;
+  }
+
+  byte value = temp - 2;
+  value = reverseBits(value);
+  value = value >> 1;
+  cmdTrame[4] = cmdTrame[4] | value;
+  if(halfDegree) {
+    cmdTrame[4] = cmdTrame[4] | B10000000;
+  }
+  return true;
+}
+
+
+
+bool setModeInTrame(byte mode) {
+  byte mask;
+
+  switch (mode) {
+    case HEAT:
+      mask = HEAT;
+      break;
+    case COOL:
+      mask = COOL;
+      break;
+    case AUTO:
+      mask = AUTO;
+      break;
+    default:
+      Serial.println("Error setMode: Unknown mode");
+      return false;
+  }
+  cmdTrame[2] = cmdTrame[2] | mask;
+  return true;
+}
+
+
+bool setPowerInTrame(bool power) {
+  byte mask = power ? B10000000 : B00000000;
+  cmdTrame[2] = cmdTrame[2] | mask;
+  return true;
+}
+
+byte generateChecksumInTrame() {
+  unsigned int total = 0;
+  for (byte i=0; i< 11; i++) {
+    total += reverseBits(cmdTrame[i]);
+  }
+  byte checksum = total % 256;
+  checksum = reverseBits(checksum);
+  cmdTrame[11] = checksum;
+  return checksum;
+}
+
+
+// void printCmdTrame() {
+//   Serial.println("--------------------");
+//   for(byte i=0; i<=11; i++) {
+//     Serial.println(cmdTrame[i], BIN);
+//   }
+//   Serial.println("--------------------");
+// }
+
 
 bool checksumIsValid(byte trame[], byte size) {
   int value = 0;
-  value = Bit_Reverse(trame[size - 1]) - checksum(trame, size);
-  //I don't know why, but there often a difference of 1 even if it seems ok... So for the
-  //moment, I accept 0 or 1 as difference.
-  return (value == 0 || value == 1);
+  value = reverseBits(trame[size - 1]) - checksum(trame, size);
+  return value == 0;
+}
+
+byte checksum(byte trame[], byte size) {
+  int total = 0;
+  for (int i = 0; i < (size - 1); i++) {
+    total += reverseBits(trame[i]);
+  }
+  return total % 256;
+}
+
+
+void publishCurrentParams() {
+  //Publish info to MQTT server
+  if(client.connected() && currentTempOut > 0) {
+    client.publish("pool/power", String(currentPower).c_str());
+    client.publish("pool/mode", String(modeToString(currentMode)).c_str());
+    client.publish("pool/temp_out", String(currentTempOut).c_str());
+    client.publish("pool/temp_prog", String(currentProgTemp).c_str());
+    Serial.println("params Published to mqtt server");
+  }
+}
+
+
+// Reverse the order of bits in a byte.
+// I.e. MSB is swapped with LSB, etc.
+byte reverseBits(unsigned char x) {
+  x = ((x >> 1) & 0x55) | ((x << 1) & 0xaa);
+  x = ((x >> 2) & 0x33) | ((x << 2) & 0xcc);
+  x = ((x >> 4) & 0x0f) | ((x << 4) & 0xf0);
+  return x;
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  delay(500);
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(mqttMsgReceivedCallBack);
+  Serial.println("Setup completed");
+
 }
 
 void loop() {
+  if (!client.connected()) {
+      MQTT_reconnect();
+      resetRecevingTrameProcess();
+  }
+  client.loop();
 
-  //IMPORTANT NOTE: On an "Arduino UNO/NANO", you don't need this because at 8Mhz, the loop of this
-  // sketch take 200 microseconds. I added this when I ported this skecth to run on an esp8266.
+
   delayMicroseconds(200);
-
-  state = digitalRead(D5);
+  state = digitalRead(PIN);
   if (!state) {
-    //End of the HIGH state
     if (highCpt) {
-      //5ms = 25 * 200us
-      if (highCpt >= 22 && highCpt <= 28) {
 
+      if (highCpt >= 22 && highCpt <= 28) { //5ms = 25 * 200us
         // START TRAME
         cptBuffer = 0;
         buffer = 0;
         wordCounter = 0;
 
-        //3ms = 15 * 200us
-      } else if (highCpt >= 12 && highCpt <= 18) {
+      } else if (highCpt >= 12 && highCpt <= 18) {  //3ms = 15 * 200us
         // BINARY O
         buffer = buffer << 1;
         cptBuffer++;
 
-        //1ms = 5 * 200us
-      } else if (highCpt >= 2 && highCpt <= 8) {
+      } else if (highCpt >= 2 && highCpt <= 8) {  //1ms = 5 * 200us
         // BINARY 1
         buffer = buffer << 1;
         buffer = buffer | 1;
@@ -130,107 +466,78 @@ void loop() {
       }
 
       if (cptBuffer == 8) {
-        //we have captured 8 bits, so this 1 byte to add in our trame array
+        //we have captured 8 bits = 1 complete byte
         trame[wordCounter] = buffer;
-        if (wordCounter < 40) { //overflow verification
+        if (wordCounter < 20) { //overflow verification
           wordCounter++;
         }
-
         cptBuffer = 0;
         buffer = 0;
       }
 
-      //reset the counter for HIGH value
-      highCpt = 0;
+      highCpt = 0;   //reset the counter for HIGH value
     }
 
   } else {
 
     highCpt++;
-    if (highCpt > 40 && wordCounter) {
+    if (highCpt > (50000 / 200) && wordCounter) {    //If HIGH more than 50ms, trame is hardly ended
 
       if (trame[0] == B01001011 && checksumIsValid(trame, wordCounter)) {
 
-        checksumIsValid(trame, wordCounter);
-        // GET THE OUT TEMP
-        unsigned char temp = Bit_Reverse(trame[4]);
+        // GET THE TEMP OUT
+        unsigned char temp = reverseBits(trame[4]);
         temp &= B00111110;
         temp = temp >> 1;
         temp = temp + 2;
 
         bool halfDegree = (trame[4] & B10000000) >> 7;
-
         float ftempOut = temp;
         if (halfDegree) {
           ftempOut = ftempOut + 0.5;
         }
-
-        temp_out = ftempOut;
-
+        currentTempOut = ftempOut;
         Serial.print("TEMP OUT:");
-        Serial.println(ftempOut);
+        Serial.println(currentTempOut);
+
+
       } else if (trame[0] == B10000001 && checksumIsValid(trame, wordCounter)) {
 
-        // GET THE TEMPERATURE PROGRAMMED
-        unsigned char temp = Bit_Reverse(trame[4]);
+        // GET THE PROGRAMMED TEMP
+        unsigned char temp = reverseBits(trame[4]);
         temp &= B00111110;
         temp = temp >> 1;
         temp = temp + 2;
-
         bool halfDegree = (trame[4] & B10000000) >> 7;
-
         float ftemp = temp;
         if (halfDegree) {
           ftemp = ftemp + 0.5;
         }
-
-        temp_programmed = ftemp;
-
+        currentProgTemp = ftemp;
         Serial.print("PROG TEMP:");
-        Serial.println(ftemp);
+        Serial.println(currentProgTemp);
 
         // CHECK THE POWER STATE
-        power = (trame[2] & B10000000) >> 7;
+        currentPower = (trame[2] & B10000000) >> 7;
         Serial.print("POWER:");
-        Serial.println(power);
-
-        // CHECK THE MODE (HEAT OR COLD)
-        mode = (trame[2] & B00001000) >> 3;
-        Serial.print("HEAT:");
-        Serial.println(mode);
+        Serial.println(currentPower);
 
         // CHECK THE AUTOMATIC MODE
-        automatic_mode = (trame[2] & B00000100) >> 2;
-        //Serial.print("AUTOMATIC:"); Serial.println(automatic_mode);
-
-
-        //Publish info to MQTT server
-        if(client.connected() && temp_out > 0) {
-          client.publish("pool/power", String(power).c_str());
-          client.publish("pool/mode", String(mode).c_str());
-          client.publish("pool/temp_out", String(temp_out).c_str());
-          client.publish("pool/temp_prog", String(temp_programmed).c_str());
-          client.publish("pool/automatic_mode", String(automatic_mode).c_str());
-          Serial.println("Published!");
+        byte automatic_mode = (trame[2] & B00000100) >> 2;
+        if(automatic_mode) {
+          currentMode = AUTO;
+        } else {
+          // CHECK HEAT OR COLD
+          boolean heat = (trame[2] & B00001000) >> 3;
+          currentMode = heat ? HEAT : COOL;
         }
+        Serial.print("MODE:");
+        Serial.println(modeToString(currentMode));
 
+
+        publishCurrentParams();
       }
       wordCounter = 0;
     }
   }
-
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-
-}
-
-// Reverse the order of bits in a byte.
-// I.e. MSB is swapped with LSB, etc.
-byte Bit_Reverse(unsigned char x) {
-  x = ((x >> 1) & 0x55) | ((x << 1) & 0xaa);
-  x = ((x >> 2) & 0x33) | ((x << 2) & 0xcc);
-  x = ((x >> 4) & 0x0f) | ((x << 4) & 0xf0);
-  return x;
 }
